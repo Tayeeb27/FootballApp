2545563aa6fba6ee8f9a6161e4bf6b04c92598ed
Tayeeb Islam <54636425+Tayeeb27@users.noreply.github.com>
2024-02-08 13:19:57 +0000

    Initial Commit

---
+++ screens/TeamDetailScreen.js
@@ -0,0 +1,21 @@
+// screens/TeamDetailScreen.js
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const TeamDetailScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Team Detail Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default TeamDetailScreen;
